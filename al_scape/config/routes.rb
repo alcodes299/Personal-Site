a66053a5f473954b04452de953d7882ceaff799d
alcodes299 <alskis299@gmail.com>
2018-03-06 18:56:34 -0500

    start of branching

--- al_scape/config/routes.rb
+++ al_scape/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get '/', to: "homes#index"
+  get '/designer', to: "homes#designer"
+  get 'coder', to: "homes#coder"
+  get 'exploration', to: "homes#Explorer"
+  get 'gammer', to: "homes#gammer"
   get '*path', to: 'homes#index'
 end
